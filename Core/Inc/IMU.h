0bfc44fc4f4e2112cb30ace9caa8af1f04fd4030
XuHaoRan <no_signal@sjtu.edu.cn>
2025-10-15 13:09:26 +0800

    feat: 增加了陀螺仪机体角速度 -> 欧拉角速度转换，使roll, pitch不为0时也能获得准确的yaw

--- Core/Inc/IMU.h
+++ Core/Inc/IMU.h
@@ -12,9 +12,23 @@ class IMU {
 public:
     void get_accel_xyz(float* accel_x, float* accel_y, float* accel_z);
     void get_gyro_xyz(float* gyro_x, float* gyro_y, float* gyro_z);
+    void get_euler_angle(float* roll, float* pitch, float* yaw);
     void update(float dt);
 
 private:
+    typedef struct EulerAngle {
+        float yaw = 0, roll = 0, pitch = 0;
+    } EulerAngle;
+
+    typedef struct Gyro {
+        float roll_rate_ = 0, pitch_rate_ = 0, yaw_rate_ = 0;
+        float last_roll_rate_ = 0, last_pitch_rate_ = 0, last_yaw_rate_ = 0;
+        float gyro_range_ = 0;
+        uint8_t rx_gyro_data_[6]{ 0 };
+
+        void gyro_calculate(const EulerAngle& euler);
+    } Gyro;
+
     typedef struct Accel {
         float x = 0, y = 0, z = 0;
         float acc_range_ = 0;
@@ -23,25 +37,11 @@ private:
         void acc_calculate();
     } Accel;
 
-    typedef struct Gyro {
-        float wx = 0, wy = 0, wz = 0;
-        float last_wx = 0, last_wy = 0, last_wz = 0;
-        float gyro_range_ = 0;
-        uint8_t rx_gyro_data_[6]{ 0 };
-
-        void gyro_calculate();
-    } Gyro;
-
-    typedef struct EulerAngle {
-        float yaw = 0, roll = 0, pitch = 0;
-    } EulerAngle;
-
     void complement_calculate(float dt, float comp_alpha_ = 0.02);
+    void kalman_calculate(float dt);
     Accel accel_;
     Gyro gyro_;
     EulerAngle euler_;
-
 };
-
 
 #endif //SPI_IMU_H
--- Core/Src/IMU.cpp
+++ Core/Src/IMU.cpp
@@ -4,7 +4,6 @@
 #include "bmi088.h"
 #include "IMU.h"
 #include <cmath>
-#include <bits/valarray_before.h>
 
 // @param dt: time interval in seconds
 void IMU::update(float dt) {
@@ -12,15 +11,14 @@ void IMU::update(float dt) {
     // 初始化
     if (!if_first) {
         accel_.acc_calculate();
-        gyro_.gyro_calculate();
-
         euler_.pitch = atan2f(accel_.y, accel_.z) * 180.0f / PI;
         euler_.roll = atan2f(-accel_.x, sqrt(accel_.y * accel_.y + accel_.z * accel_.z)) * 180.8f / PI; // 加速度计单位为 mg
         euler_.yaw = 0;
+        gyro_.gyro_calculate(this->euler_);
         if_first = true;
     }
     accel_.acc_calculate();
-    gyro_.gyro_calculate();
+    gyro_.gyro_calculate(this->euler_);
     complement_calculate(dt);
 }
 
@@ -31,9 +29,14 @@ void IMU::get_accel_xyz(float* accel_x, float* accel_y, float* accel_z) {
 }
 
 void IMU::get_gyro_xyz(float* gyro_x, float* gyro_y, float* gyro_z) {
-    *gyro_x = gyro_.wx;
-    *gyro_y = gyro_.wy;
-    *gyro_z = gyro_.wz;
+    *gyro_x = gyro_.roll_rate_;
+    *gyro_y = gyro_.pitch_rate_;
+    *gyro_z = gyro_.yaw_rate_;
+}
+void IMU::get_euler_angle(float* roll, float* pitch, float* yaw) {
+    *roll = euler_.roll;
+    *pitch = euler_.pitch;
+    *yaw = euler_.yaw;
 }
 
 void IMU::Accel::acc_calculate() {
@@ -67,9 +70,10 @@ void IMU::Accel::acc_calculate() {
         pow(2, raw_range + 1) * 1.5f;
 }
 
-void IMU::Gyro::gyro_calculate() {
+void IMU::Gyro::gyro_calculate(const EulerAngle& euler) {
     // 1. 设置/读取acc0x0F寄存器中的量程range参数，并换算为量程系数
-    uint8_t raw_range;
+    static uint8_t raw_range;
+    static float transition_matrix[3][3] = { 0 };
     bmi088_gyro_read_reg(0x0F, &raw_range, 1);
     switch (raw_range) {
         case 0x00:
@@ -92,22 +96,40 @@ void IMU::Gyro::gyro_calculate() {
     }
     // 2. 读取acc0x12寄存器中的6位gyro数据
     bmi088_gyro_read_reg(0x02, rx_gyro_data_, 6);
-    // 3. 用量程系数将原始数据转换为常用单位 并使用中值积分算出角度
-    last_wx = wx;
-    last_wy = wy;
-    last_wz = wz;
-    wx = static_cast<int16_t>(rx_gyro_data_[1] << 8 | rx_gyro_data_[0]) / 32767.0f * gyro_range_;
-    wy = static_cast<int16_t>(rx_gyro_data_[3] << 8 | rx_gyro_data_[2]) / 32767.0f * gyro_range_;
-    wz = static_cast<int16_t>(rx_gyro_data_[5] << 8 | rx_gyro_data_[4]) / 32767.0f * gyro_range_;
+    // 3. 用量程系数将原始数据转换为常用单位 并使用中值积分算出角度 注意坐标系变换
+    last_roll_rate_ = roll_rate_;
+    last_pitch_rate_ = pitch_rate_;
+    last_yaw_rate_ = yaw_rate_;
+
+    float roll_rad = euler.roll * PI / 180.0f;
+    float pitch_rad = euler.pitch * PI / 180.0f;
+    transition_matrix[0][0] = 1;
+    transition_matrix[0][1] = sinf(roll_rad) * tanf(pitch_rad);
+    transition_matrix[0][2] = cosf(roll_rad) * tanf(pitch_rad);
+    transition_matrix[1][0] = 0;
+    transition_matrix[1][1] = cosf(roll_rad);
+    transition_matrix[1][2] = -sinf(roll_rad);
+    transition_matrix[2][0] = 0;
+    transition_matrix[2][1] = sinf(roll_rad) / cosf(pitch_rad);
+    transition_matrix[2][2] = cosf(roll_rad) / cosf(pitch_rad);
+    float imu_wx = static_cast<int16_t>(rx_gyro_data_[1] << 8 | rx_gyro_data_[0]) / 32767.0f * gyro_range_;
+    float imu_wy = static_cast<int16_t>(rx_gyro_data_[3] << 8 | rx_gyro_data_[2]) / 32767.0f * gyro_range_;
+    float imu_wz = static_cast<int16_t>(rx_gyro_data_[5] << 8 | rx_gyro_data_[4]) / 32767.0f * gyro_range_;
+    roll_rate_ = transition_matrix[0][0] * imu_wx + transition_matrix[0][1] * imu_wy + transition_matrix[0][2] * imu_wz;
+    pitch_rate_ = transition_matrix[1][0] * imu_wx + transition_matrix[1][1] * imu_wy + transition_matrix[1][2] *
+        imu_wz;
+    yaw_rate_ = transition_matrix[2][0] * imu_wx + transition_matrix[2][1] * imu_wy + transition_matrix[2][2] * imu_wz;
 }
 
 void IMU::complement_calculate(float dt, float comp_alpha_) {
     float acc_pitch = atan2f(-accel_.x, sqrt(accel_.y * accel_.y + accel_.z * accel_.z)) * 180.0f / PI;
     float acc_roll = atan2f(accel_.y, accel_.z) * 180.0f / PI;
-    float gyro_pitch = euler_.pitch + (gyro_.last_wy + gyro_.wy) / 2.0f * dt; // 中值积分
-    float gyro_roll = euler_.roll + (gyro_.last_wx + gyro_.wx) / 2.0f * dt;
+    float gyro_pitch = euler_.pitch + (gyro_.last_pitch_rate_ + gyro_.pitch_rate_) / 2.0f * dt; // 中值积分
+    float gyro_roll = euler_.roll + (gyro_.last_roll_rate_ + gyro_.roll_rate_) / 2.0f * dt;
 
-    euler_.yaw += (gyro_.last_wz + gyro_.wz) / 2.0f * dt; // yaw角不使用互补滤波
+    euler_.yaw += (gyro_.last_yaw_rate_ + gyro_.yaw_rate_) / 2.0f * dt; // yaw角不使用互补滤波
     euler_.pitch = (1.0f - comp_alpha_) * gyro_pitch + comp_alpha_ * acc_pitch;
     euler_.roll = (1.0f - comp_alpha_) * gyro_roll + comp_alpha_ * acc_roll;
 }
+
+void IMU::kalman_calculate(float dt) {}
